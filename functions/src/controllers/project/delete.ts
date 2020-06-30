6c4d7e51addeda77b7ceb0ddc51254cfe0177066
Scott Wang <scotteau@gmail.com>
2020-06-30 21:33:12 +0800

    implemented batch delete columns & task when deleting a project

--- functions/src/controllers/project/delete.ts
+++ functions/src/controllers/project/delete.ts
@@ -6,18 +6,43 @@ const deleteProject = async (ctx: any) => {
 
     try {
         const workspaceRef = db.doc(`/workspaces/${workspaceId}`);
-        await workspaceRef.update({
+        const batch = db.batch();
+
+        batch.update(workspaceRef, {
             projectOrder: admin.firestore.FieldValue.arrayRemove(projectId),
         });
 
-        await db.doc(`/projects/${projectId}`).delete();
+        const projectRef = db.doc(`/projects/${projectId}`);
+        batch.delete(projectRef);
+
+        const tasksRef = await db
+            .collection("/tasks")
+            .where("projectIds", "array-contains", projectId)
+            .get();
+
+        tasksRef.docs.forEach((doc: any) => {
+            batch.delete(doc.ref);
+        });
+
+        const columnsRef = await db
+            .collection("/columns")
+            .where("projectId", "==", projectId)
+            .get();
+
+        columnsRef.docs.forEach((doc: any) => {
+            batch.delete(doc.ref);
+        });
+
+        await batch.commit();
+
         ctx.body = {
             message: `project ${projectId} is successfully deleted.`,
+            ref: tasksRef,
         };
     } catch (e) {
         ctx.status = e.status || 500;
         ctx.body = {
-            message: `Something went wrong! ${e.error}`,
+            message: e.message,
         };
     }
 };
