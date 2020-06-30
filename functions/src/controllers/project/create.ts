13f59284a484ffffb0014c179df5e1faa321b7f4
Scott Wang <scotteau@gmail.com>
2020-06-30 19:59:55 +0800

    implemented - POST /projects

--- functions/src/controllers/project/create.ts
+++ functions/src/controllers/project/create.ts
@@ -1,6 +1,6 @@
 import * as admin from "firebase-admin";
-
 const createProject = async (ctx: any) => {
+    const db = ctx.db;
     const {
         name,
         colorIndex,
@@ -10,39 +10,32 @@ const createProject = async (ctx: any) => {
         id,
     } = ctx.req.body;
 
-    ctx.body = {
-        name,
-        colorIndex,
-        id,
-    };
-    // const db = ctx.db;
-    //
-    // try {
-    //     await db.collection("projects").doc(id).set({
-    //         name,
-    //         colorIndex,
-    //         iconIndex,
-    //         createdOn: admin.firestore.Timestamp.now(),
-    //         columnOrder: [],
-    //         workspace,
-    //         activeUsers: [],
-    //     });
-    //
-    //     await db.doc(`/workspaces/${workspace}`).update({
-    //         projectOrder: [...projectOrder, id],
-    //     });
-    //
-    //     ctx.status = 201;
-    //     ctx.body = {
-    //         message: `Project - ${id} is created`,
-    //     };
-    // } catch (e) {
-    //     console.log(e);
-    //     ctx.status = e.status || 500;
-    //     ctx.body = {
-    //         message: e.message,
-    //     };
-    // }
+    try {
+        await db.collection("projects").doc(id).set({
+            name,
+            colorIndex,
+            iconIndex,
+            createdOn: admin.firestore.Timestamp.now(),
+            columnOrder: [],
+            workspace,
+            activeUsers: [],
+        });
+
+        await db.doc(`/workspaces/${workspace}`).update({
+            projectOrder: [...projectOrder, id],
+        });
+
+        ctx.status = 201;
+        ctx.body = {
+            message: `Project - ${id} is created`,
+        };
+    } catch (e) {
+        console.log(e);
+        ctx.status = e.status || 500;
+        ctx.body = {
+            message: e.message,
+        };
+    }
 };
 
 export default createProject;
